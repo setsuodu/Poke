dc4d909e4c5dfd25557c71b29cbc6db4848ce260
Setsuodu <339131001@qq.com>
2017-03-02 00:29:06 +0800

    fix unicode bug
    Use JsonUtility to Serialize the result.

--- Assets/ShareSDK/ShareDemo.cs
+++ Assets/ShareSDK/ShareDemo.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
-using cn.sharesdk.unity3d; //导入ShareSdk
+using cn.sharesdk.unity3d; //导入ShareSDK
 
 public class ShareDemo : MonoBehaviour
 {
     private ShareSDK shareSdk;
     public Text message;
+    QQUser qqUser = new QQUser();
 
     void Start()
     {
         shareSdk = GetComponent<ShareSDK>();
-
-        //分享回调事件
-        shareSdk.shareHandler += ShareResultHandler;
-        //授权回调事件
-        shareSdk.authHandler += AuthResultHandler;
-        //用户信息事件
-        shareSdk.showUserHandler += GetUserInfoResultHandler;
+        shareSdk.shareHandler += ShareResultHandler; //分享回调事件
+        shareSdk.authHandler += AuthResultHandler; //授权回调事件
+        shareSdk.showUserHandler += GetUserInfoResultHandler; //用户信息事件
     }
 
-    //分享
+    public void LoadScene(string sc)
+    {
+        Application.LoadLevel(sc);
+    }
+
+    #region 分享
+
     public void OnShareClick()
     {
         ShareContent content = new ShareContent();
@@ -42,28 +46,29 @@ public class ShareDemo : MonoBehaviour
 
     // 分享结果回调
     void ShareResultHandler(int reqID, ResponseState state, PlatformType type, Hashtable result)
-    {   //成功
-        if (state == ResponseState.Success)
+    {   
+        if (state == ResponseState.Success) //成功
         {
-            message.text = ("share result :");
-            message.text = (MiniJSON2.jsonEncode(result));
+            message.text = "share result :";
+            message.text = MiniJSON2.jsonEncode(result);
         }
-        //失败
-        else if (state == ResponseState.Fail)
+        else if (state == ResponseState.Fail) //失败
         {
-            message.text = ("fail! error code = " + result["error_code"] + "; error msg = " + result["error_msg"]);
+            message.text = "fail! error code = " + result["error_code"] + "; error msg = " + result["error_msg"];
         }
-        //关闭
-        else if (state == ResponseState.Cancel)
+        else if (state == ResponseState.Cancel) //取消，按下返回键
         {
-            message.text = ("cancel !");
+            message.text = "cancel !";
         }
     }
 
-    //授权
+    #endregion
+
+    #region 登陆授权
+
     public void OnAuthClick()
     {
-        //请求QQ授权//请求这个授权是为了获取用户信息来第三方登录
+        //请求QQ授权，请求这个授权是为了获取用户信息来第三方登录
         shareSdk.Authorize(PlatformType.QQ);
     }
 
@@ -72,18 +77,16 @@ public class ShareDemo : MonoBehaviour
     {
         if (state == ResponseState.Success)
         {
-            message.text = ("authorize success !");
-
-            //授权成功的话，获取用户信息
-            shareSdk.GetUserInfo(type);
+            message.text = "authorize success !";
+            shareSdk.GetUserInfo(type); //授权成功的话，获取用户信息
         }
         else if (state == ResponseState.Fail)
         {
-            message.text = ("fail! error code = " + result["error_code"] + "; error msg = " + result["error_msg"]);
+            message.text = "fail! error code = " + result["error_code"] + "; error msg = " + result["error_msg"];
         }
         else if (state == ResponseState.Cancel)
         {
-            message.text = ("cancel !");
+            message.text = "cancel !";
         }
     }
 
@@ -92,12 +95,20 @@ public class ShareDemo : MonoBehaviour
     {
         if (state == ResponseState.Success)
         {
-
-            //获取成功的话 可以写一个类放不同平台的结构体，用PlatformType来判断，用户的Json转化成结构体，来做第三方登录。
+            //获取成功的话，可以写一个类放不同平台的结构体，用PlatformType来判断，用户的Json转化成结构体，来做第三方登录。
             switch (type)
             {
                 case PlatformType.QQ:
                     message.text = (MiniJSON2.jsonEncode(result));  //Json
+
+                    //完整的json内容作为string传入，得到整个UserInfo结构/类
+                    qqUser = JsonUtility.FromJson<QQUser>(MiniJSON2.jsonEncode(result));
+                    message.text = qqUser.nickname;
+                    Debug.Log("nikename : " + qqUser.nickname);
+                    //php数据库交互，如果数据库里没有，新建用户，→登陆。
+                    //如果有，→登陆。
+                    //ToDo it...
+
                     break;
             }
         }
@@ -111,33 +122,31 @@ public class ShareDemo : MonoBehaviour
         }
     }
 
-    public void LoadScene(string sc)
+    #endregion
+
+    #region JSON 序列化
+
+    [Serializable]
+    struct QQUser //QQ用户信息结构体
     {
-        Application.LoadLevel(sc);
+        public string yellow_vip_level;
+        public string msg;
+        public string province;
+        public string gender;
+        public string is_yellow_year_vip;
+        public int is_lost;
+        public string nickname;
+        public int ret;
+        public string level;
+        public string city;
+        public string figureurl;
+        public string figureurl_1;
+        public string figureurl_2;
+        public string figureurl_qq_1;
+        public string figureurl_qq_2;
+        public string vip;
+        public string is_yellow_vip;
     }
-}
 
-/*
-
-//QQ用户信息结构体
- struct QQUser 
-{
-     public string yellow_vip_level;
-     public string msg;
-     public string province;
-     public string gender;
-     public string is_yellow_year_vip;
-     public int is_lost;
-     public string nickname;
-     public int ret;
-     public string level;
-     public string city;
-     public string figureurl;
-     public string figureurl_1;
-     public string figureurl_2;
-     public string figureurl_qq_1;
-     public string figureurl_qq_2;
-     public string vip;
-     public string is_yellow_vip;
+    #endregion
 }
-*/
