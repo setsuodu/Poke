b5e26c4d5b6aa3733320fd92a863a8ab674f903d
Setsuodu <339131001@qq.com>
2017-03-03 01:31:31 +0800

    Conversation
    1. add Conversation.
    2. add button sound.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public List<string> ConversationList;
+    public Text Conversation;
+    public int count = 0;
+    public AudioSource light;
+
+    void Start()
+    {
+        StartCoroutine(PlaySound(light, 2f));
+    }
+
+    public void NextWord()
+    {
+        if (count < ConversationList.Count)
+        {
+            Conversation.text = ConversationList[count];
+            count += 1;
+        }
+    }
+
+	public void LoadScene (string sc)
+    {
+        if (count < ConversationList.Count) return;
+        else Application.LoadLevel(sc);
+	}
+
+    IEnumerator PlaySound(AudioSource _audio, float _time)
+    {
+        yield return new WaitForSeconds(_time);
+        _audio.Play();
+    }
+}
--- Assets/Scripts/UIFade.cs
+++ Assets/Scripts/UIFade.cs
@@ -2,67 +2,29 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public enum FadeType
-{
-    UIFadeIn,
-    UIFadeOut
-}
-
 public class UIFade : MonoBehaviour
 {
     private Graphic g;
-    public FadeType myFadeType = FadeType.UIFadeIn; //选择Fade类型
     public float delay, duration;
+    public Color fromColor, toColor;
 
     void Awake() // 各种初始化
     {
         g = GetComponent<Graphic>(); // UI.Graphic 视觉UI组件的基类。
-
-        switch (myFadeType)
-        {
-            case FadeType.UIFadeIn:
-                g.GetComponent<CanvasRenderer>().SetAlpha(0f);
-                break;
-            case FadeType.UIFadeOut:
-                g.GetComponent<CanvasRenderer>().SetAlpha(1f);
-                break;
-        }
+        g.GetComponent<CanvasRenderer>().SetColor(fromColor);
     }
 
     void Start() // 执行
     {
         if (Application.isPlaying)
         {
-            StartCoroutine(FadeIn(delay));
+            StartCoroutine(Fade(delay));
         }
     }
 
-    IEnumerator FadeIn(float _time)
+    IEnumerator Fade(float _time)
     {
         yield return new WaitForSeconds(_time);
-        g.CrossFadeAlpha(1f, duration, false);
+        g.CrossFadeColor(toColor, duration, false, true);
     }
-
-    IEnumerator Fadeout(float _time)
-    {
-        yield return new WaitForSeconds(_time);
-        g.CrossFadeAlpha(0f, duration, false);
-    }
-
-    #region 递归遍历子物体
-    void Example(GameObject obj)
-    {
-        Recursive(obj);
-    }
-
-    private void Recursive(GameObject parentGameObject)
-    {
-        //Do something you want...
-        foreach (Transform child in parentGameObject.transform)
-        {
-            Recursive(child.gameObject);
-            g = child.GetComponent<Graphic>();
-        }
-    }
-    #endregion
 }
